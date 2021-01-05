a7de3f755e36dc7107931cfaa0db81056cd5b522
Fatih ÜNLÜ <unlu.fa@gmail.com>
2021-01-05 22:51:40 +0300

    vuetify & composition api imported

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+
+// Vuetify doesn't fully support vue3,
+import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,10 @@
 </template>
 
 <script>
+
+// use the APIs
+// import { ref, reactive } from '@vue/composition-api'
+
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
